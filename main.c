99f7ff9c37bb9ae110ed80384f761f3247f7f442
André Favotto <andrefavotto@hotmail.com>
2022-01-23 20:41:30 -0300

    Criado repositorio

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+  int a = 1;
+  int b = 1;
+  int c = a + b;
+  printf("Hello World, the result is: %d \n", c);
+
+  printf("Alteração de qualquer coisa no código \n");
+
+  return EXIT_SUCCESS;
+}
